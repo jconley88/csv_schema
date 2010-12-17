3031882ad76ef1cb95dc951a5b8b9b3a34decee7
jconley <conley.jj@gmail.com>
2010-12-17 18:27:36 -0500

    Indexed header fields by column number to allow retrieval of fields by field name when FasterCSV is opened with :headers => false

--- bin/csv_schema.rb
+++ bin/csv_schema.rb
@@ -14,8 +14,8 @@ class CSVSchema
     @unique_fields = {}
     @cant_be_nil_fields = []
     @restrict_value_fields = {}
-    @field_requirements = args[:field_requirements] || []
-    @field_requirements.each do |field, requirements|
+    field_requirements = args[:field_requirements] || []
+    field_requirements.each do |field, requirements|
       @unique_fields[field] = [] if requirements[:unique]
       @cant_be_nil_fields << field if requirements[:cant_be_nil]
       @restrict_value_fields[field] = requirements[:restrict_values] if requirements[:restrict_values]
@@ -24,14 +24,14 @@ class CSVSchema
 
   def validate()
     raise "#{@file} cannot be found" unless File.exists?(@file)
-    header_row = true
     @current_row = 1
     FasterCSV.foreach(@file) do |row|
-      if header_row
+      if @current_row == 1
+        @headers = row
+        index_field_requirements_by_column_number
         validate_duplicate_headers(row, @headers_transform) unless @allow_duplicate_headers
         validate_blank_headers(row) unless @allow_blank_headers
         validate_required_headers(row) if @required_headers
-        header_row = false
       else
         validate_blank_rows(row) unless @allow_blank_rows
         validate_restrict_value_fields(row) if !@restrict_value_fields.empty?
@@ -45,6 +45,32 @@ class CSVSchema
   end
 
 private
+  def index_field_requirements_by_column_number
+    headers_to_column_number = {}
+    @headers.each_with_index do |header, column_number|
+      headers_to_column_number[header] = column_number
+    end
+    @indexed_headers = headers_to_column_number.invert
+
+    cbn = []
+    @cant_be_nil_fields.each do |field|
+      cbn << headers_to_column_number[field]
+    end
+    @cant_be_nil_fields = cbn
+
+    rv = {}
+    @restrict_value_fields.each do |field, restrict_values|
+      rv[headers_to_column_number[field]] = restrict_values
+    end
+    @restrict_value_fields = rv
+
+    u = {}
+    @unique_fields.each do |field, unique|
+      u[headers_to_column_number[field]] = unique
+    end
+    @unique_fields = u
+  end
+
   def validate_duplicate_headers(row, transform = nil)
     headers = transform ? row.map{ |header| transform.call(header)} : row
     dups = headers.inject(Hash.new(0)) { |h, v| h[v] += 1; h }.reject { |k, v| v==1 }.keys #http://snippets.dzone.com/posts/show/3838
@@ -72,8 +98,8 @@ private
   end
 
   def validate_restrict_value_fields(row)
-    @restrict_value_fields.each do |field, allowed_values|
-      raise "The '#{field}' column contains an illegal value: '#{row[field]}'" unless allowed_values.include?(row[field])
+    @restrict_value_fields.each do |col_num, allowed_values|
+      raise "The '#{header_name(col_num)}' column contains an illegal value: '#{row[col_num]}'" unless allowed_values.include?(row[col_num])
     end
   end
 
@@ -87,5 +113,9 @@ private
 
   def validate_unique_fields
     
+  end
+
+  def header_name(col_num)
+    @indexed_headers[col_num]
   end
 end
